3e61408cb2c6a925873520360cc81cf260b48180
MiguelCoba0502 <miguel_8090@hotmail.com>
2024-12-19 22:41:02 -0600

    Initial Commit

---
+++ MemoryAnimals/MemoryAnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryAnimalsApp.swift
+//  MemoryAnimals
+//
+//  Created by Eduardo Coba on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryAnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
